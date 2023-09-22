82f38da738faebf97c848f425346e7e232febf7e
buggasai <buggasai@gmail.com>
2023-09-22 15:34:26 +0530

    redux added

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -6,10 +6,10 @@ export function Menu() {
     <div className='navbar container-fluid flex justify-content-center'>
       <div className='navbar-inner'>
         <ul className='nav flex-column'>
-              <li className={`me-3 nav-item p-2 liClass`}><Link href='/home'>Home</Link> </li>
-              <li className={`me-3 nav-item  liClass p-2`}><Link href='/about'>About</Link></li>
-              <li className={`me-3 nav-item liClass p-2`}><Link href='/contact'>Contact Us</Link></li>
-              <li className={`me-3 nav-item liClass p-2`}><Link href='/redux'>Redux</Link></li>
+              <li className={`btn btn-danger`}><Link className='text-white' href='/home'>Home</Link> </li>
+              <li className={`mt-3 btn btn-danger`}><Link className='text-white' href='/about'>About</Link></li>
+              <li className={`mt-3 btn btn-danger`}><Link className='text-white' href='/contact'>Contact Us</Link></li>
+              <li className={`mt-3 btn btn-danger`}><Link className='text-white' href='/redux'>Redux</Link></li>
           </ul>
       </div>
     </div>
